7640e22e26a17e304e1a37d2d04819efd4017956
Valentine Prokopenko <xxrazorxx181102@gmail.com>
2022-04-05 21:50:38 +0300

    start

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        textTransform: 'uppercase',
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
+
+
